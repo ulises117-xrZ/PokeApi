9a53d3a73d2fc2da63768c397f583bab30c17bf3
Ulises <rollercruz60@gmail.com>
2021-05-03 01:00:36 -0600

    optimizando...

--- js/fetch.js
+++ js/fetch.js
@@ -1,56 +1,45 @@
-//funcion para insertar nombre 
-const nombresPokemon = (clase, nombre)=>document.querySelector(clase).innerHTML = nombre;
-
-//funciones para insertar El tipo de pokemon
-const poderesPokemon = (clase, poder)=> document.querySelector(clase).innerHTML = poder;
-
-//funciones para insertar Imagen del pokemon
-const imagenesPokemon = (clase, imagen)=> document.querySelector(clase).setAttribute("src", imagen);
-
-
-function peticiones(numeroPoke,nombre, poder, imagen) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${numeroPoke}/`)
+//Llamado a la PokeApi con 20 pokemons
+function peticionTodos(){
+    fetch(`https://pokeapi.co/api/v2/pokemon`)
     .then(response => response.json())
-    .then(pokemon2=>{
-        debugger
-        nombresPokemon(nombre, pokemon2.name);
-        poderesPokemon(poder, pokemon2.types[0].type.name);
-        imagenesPokemon(imagen, pokemon2.sprites.front_default);
+    .then(pokemon3=>{
+        pokemon3.results.forEach(element => {//pa recorrer todos los urls
+            peticionPokemon(element.url)//extraer cada url
+        });
+        
     })
 }
 
-peticiones(25, ".m_nombre", ".m_poder", ".m_pokemon_i");
-peticiones(26, ".m_nombre2", ".m_poder2", ".m_pokemon_i2");
-peticiones(27, ".m_nombre3", ".m_poder3", ".m_pokemon_i3");
-peticiones(28, ".m_nombre4", ".m_poder4", ".m_pokemon_i4");
+
+function  peticionPokemon(urlPokemon){
+    fetch(`${urlPokemon}`)//insertar todos los urls
+    .then(response => response.json())
+    .then(pokemonUnico=>{
+        agregarPokemon(
+            pokemonUnico.name,//el nombre del pokemon
+            pokemonUnico.types[0].type.name,//el tipo de pokemon
+            pokemonUnico.sprites.front_default);//la imagen del pokemon
+    })
+}
+
+function agregarPokemon(nombre, tipo, sprites) {//aca defino parametros para modif. el html
+    const fragment = document.createDocumentFragment();//cree un fragmento para que sea mas optimo
+    for (let i = 0; i < 1; i++){//loop que por cada url me imprime este codigo
+        const div = document.createElement('div');
+        div.innerHTML = ` <div class="pokemon pokemon">
+        <img src="${sprites}" alt="Imagen del pokachi" class="m_pokemon_i">
+            <div class="propiedades">
+            <p>Nombre: <span class="m_nombre n1">${nombre}</span></p>    
+            <p>Tipo: <span class="m_poder p1">${tipo}</span></p>
+            </div>
+        </div>`;
+        fragment.appendChild(div);//agrego el div al fragmento
+    }
+    
+    document.body.appendChild(fragment);//agrego el fragmento al body (html)
+}
+
+peticionTodos();
 
 
 
-
-
-
-
-
-
-
-
-
-//fetch es para llamar y conectar apis
-//informacion de pikachu
-// fetch('https://pokeapi.co/api/v2/pokemon/25/')
-//     .then(response => response.json())
-//     .then(pokemon =>{
-//         // debugger
-//         nombresPokemon(".m_nombre",pokemon.name);
-//         poderesPokemon(".m_poder", pokemon.types[0].type.name);
-//         imagenesPokemon(".m_pokemon_i", pokemon.sprites.front_default);
-//     })
-// //informacion de raichu
-// fetch('https://pokeapi.co/api/v2/pokemon/26/')
-//     .then(response => response.json())
-//     .then(pokemon2=>{
-//         nombresPokemon(".m_nombre1", pokemon2.name);
-//         // debugger
-//         poderesPokemon(".m_poder1", pokemon2.types[0].type.name);
-//         imagenesPokemon(".m_pokemon_i1", pokemon2.sprites.front_default);
-//     })
