d8391a28b8024c92e9a3b0c41423bdc55d94216f
Ulises <rollercruz60@gmail.com>
2021-05-09 08:33:47 -0600

    problemas con fetch

--- js/fetch.js
+++ js/fetch.js
@@ -1,45 +1,20 @@
-//Llamado a la PokeApi con 20 pokemons
-function peticionTodos() {
-    fetch(`https://pokeapi.co/api/v2/pokemon?offset=20&limit=20`)
-        .then(response => response.json())
-        .then(pokemon3 => {
-            pokemon3.results.forEach(element => {//pa recorrer todos los urls
-                peticionPokemon(element.url)//extraer cada url
-            });
+async function ObtenerPokemons() {
+    let peticion = await fetch('https://pokeapi.co/api/v2/type')
+    let datos = await peticion.json();
+    recorrerArreglo(datos.results)
+}
 
+function pokemons(resultado) {
+    fetch(`${resultado.url}`);
+    
+}
+
+function recorrerArreglo(resultados) {
+        resultados.forEach(hola => {
         })
+
+
+
 }
-
-
-function peticionPokemon(urlPokemon) {
-    fetch(`${urlPokemon}`)//insertar todos los urls
-        .then(response => response.json())
-        .then(pokemonUnico => {
-            agregarPokemon(
-                pokemonUnico.name,//el nombre del pokemon
-                pokemonUnico.types[0].type.name,//el tipo de pokemon
-                pokemonUnico.sprites.front_default);//la imagen del pokemon
-        })
-}
-
-function agregarPokemon(nombre, tipo, sprites) {//aca defino parametros para modif. el html
-    const fragment = document.createDocumentFragment();//cree un fragmento para que sea mas optimo
-    const div = document.createElement('div');
-    div.className = "pokemon"
-    div.innerHTML = 
-        `
-            <img src="${sprites}" alt="Imagen del pokachi">
-            <div class="propiedades">
-                <p>Nombre: <span class="m_nombre n1">${nombre}</span></p>    
-                <p>Tipo: <span class="m_poder p1">${tipo}</span></p>
-            </div>
-        `;
-    fragment.appendChild(div);//agrego el div al fragmento
-    const principalDiv = document.querySelector(".pokemon-container")
-    principalDiv.appendChild(fragment);//agrego el fragmento al body (html)
-}
-
-peticionTodos();
-
-
-
+// pokemons()
+ObtenerPokemons()
