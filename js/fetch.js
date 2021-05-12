9e05700b1b06eb9fc25d11ba329377cb546a41ce
Ulises <rollercruz60@gmail.com>
2021-05-12 10:27:43 -0600

    buscador Interno version 1.0

--- js/fetch.js
+++ js/fetch.js
@@ -1,14 +1,34 @@
 const poke_container = document.getElementById('poke_container');
-const pokemons_number = 10;
+const pokemons_number = 20;
+const inputSearch = document.querySelector('.form-control');
+const button = document.querySelector('.btnsearch');
+const resultados = document.querySelector('.pokemonResultante');
 
 
+
+//recorrer todos los pokemon de acuerdo al numero en
+//la variable pokemons_number
 const llamarPokemons = async () => {
-    for (let i = 1; i < pokemons_number; i++){
-        await getPokemon(i);
-    }
+        for (let i = 1; i < pokemons_number; i++) {
+            await getPokemon(i);
+    } 
 }
 
-const getPokemon = async id => {
+button.addEventListener('click', () => {
+    getForResults(inputSearch.value);
+})
+
+async function getForResults(id2) {
+    const resultado = await fetch(`https://pokeapi.co/api/v2/pokemon/${id2}`)
+    const listaPokemons = await resultado.json();
+    mostrarBusqueda(listaPokemons.name,
+        listaPokemons.types[0].type.name,
+        listaPokemons.sprites.front_shiny,
+        listaPokemons.abilities[0].ability.name,
+        listaPokemons.id)
+}
+
+async function getPokemon(id){
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const respuesta = await fetch(url);
     const pokemon = await respuesta.json();
@@ -16,9 +36,9 @@ const getPokemon = async id => {
         pokemon.types[0].type.name,
         pokemon.sprites.front_shiny,
         pokemon.abilities[0].ability.name,
-        pokemon.id)
-    
+        pokemon.id);
 }
+    
 
 function showInCard(nombre, type, sprite, hability, id) {
     let fragmento = document.createDocumentFragment();
@@ -36,8 +56,14 @@ function showInCard(nombre, type, sprite, hability, id) {
     const contenedor = document.getElementById('poke_container');
     contenedor.appendChild(fragmento);
 
-    
 }
 
-
+function mostrarBusqueda(nombre, type, sprite, hability, id) {
+    document.querySelector('.resultados').classList.toggle('activo')
+    document.querySelector('.nombrePokemon').textContent = nombre;
+    document.querySelector('.type').textContent = type;
+    document.querySelector('.pokemonImagen').setAttribute('src', `${sprite}`);
+    document.querySelector('.hability').textContent = hability;
+    document.querySelector('.numeroId').textContent = `${id}`;
+}
 llamarPokemons();
